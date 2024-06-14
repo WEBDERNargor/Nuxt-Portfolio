fa58d46d00f51a20b68483d2e63f67b778fadd84
WEBDERNargor <tomditsarut2@gmail.com>
2024-06-14 19:32:43 +0700

    new file

--- components/Skillcomponent.vue
+++ components/Skillcomponent.vue
@@ -4,7 +4,7 @@
     </div>
 
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 gap-4">
-        <div v-for="(row, key) in skills">
+        <div v-for="(row, key) in skills" :key="key">
 
 
 
